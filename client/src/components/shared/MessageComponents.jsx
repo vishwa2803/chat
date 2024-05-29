051b46d4efdc11ab4957f243acd445119eb0951d
vishwa satasiya <vishwasatasiya@gmail.com>
2024-05-29 17:56:02 +0530

    motion add in message

--- client/src/components/shared/MessageComponents.jsx
+++ client/src/components/shared/MessageComponents.jsx
@@ -1,56 +1,59 @@
-import { Box, Typography } from '@mui/material';
-import React, { memo } from 'react'
-import { lightBlue } from '../../constants/color';
-import moment from 'moment';
+import { Box, Typography } from "@mui/material";
+import React, { memo } from "react";
+import { lightBlue } from "../../constants/color";
+import moment from "moment";
+import {motion} from "framer-motion";
 
-const MessageComponents = ({ message, user}) => {
+const MessageComponents = ({ message, user }) => {
+  const { sender, content, attachments = [], createdAt } = message;
 
-    const {sender, content, attachmenta =[], createdAt} = message;
+  const sameSender = sender._id === user._id;
 
-    const sameSender = sender._id === user._id;
-
-    const timeAgo = moment(createdAt).fromNow();
+  const timeAgo = moment(createdAt).fromNow();
 
   return (
-    <div
+    <motion.div
+    initial={{ opacity: 0, x: "-100%" }}
+    whileInView={{ opacity: 1, x: 0 }}
     style={{
-        alignSelf: sameSender ? "flex-end" : "flex-start",
-        backgroundColor:"white",
-        color: "black",
-        borderRadius:"5px",
-        padding: "0.5rem",
-        width: "fit-content",
-        
+      alignSelf: sameSender ? "flex-end" : "flex-start",
+      backgroundColor: "white",
+      color: "black",
+      borderRadius: "5px",
+      padding: "0.5rem",
+      width: "fit-content",
     }}
     >
-      {!sameSender && <Typography color={lightBlue} fontWeight={"600"} variant='caption'>{sender.name}</Typography>}
-    {content && <Typography>{content}</Typography>}
+      {!sameSender && (
+        <Typography color={lightBlue} fontWeight={"600"} variant="caption">
+          {sender.name}
+        </Typography>
+      )}
+      {content && <Typography>{content}</Typography>}
 
-{
-    attechments.length > 0 && 
-        attechments.map((attechment, index) => {
-            const url = attachment.url;
-            const file = "asd";
+      {attachments.length > 0 &&
+        attachments.map((attachment, index) => {
+          const url = attachment.url;
+          const file = "asd";
 
-            return (
-                <Box key={index}>
-                    <a
-                    href=''
-                    target='_blank'
-                    download
-                    style={{
-                        color:"black",
-                    }}
-                    ></a>
-                </Box>
-            )
-
-        }
-    )
-}
-    <Typography variant='caption' color={"text.secondary"}>{timeAgo}</Typography>
-    </div>
-  )
-}
+          return (
+            <Box key={index}>
+              <a
+                href=""
+                target="_blank"
+                download
+                style={{
+                  color: "black",
+                }}
+              ></a>
+            </Box>
+          );
+        })}
+      <Typography variant="caption" color={"text.secondary"}>
+        {timeAgo}
+      </Typography>
+    </motion.div>
+  );
+};
 
 export default memo(MessageComponents);
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -63,6 +63,21 @@ export const sampleMessage = [
         },
         chat: "chatId",
         createdAt: "2024-05-29T10:41:30.630Z",
+      },{
+        attachments: [
+            {
+                public_id: "asdsad 2",
+                url: "https://www.w3schools.com/howto/img_avatar.png",
+              },
+        ],
+        content: "heyyyy",
+        _id: "sfnsdjkfsdnfkjsbnd",
+        sender: {
+          _id: "user._id",
+          name: "chat 1 ",
+        },
+        chat: "chatId",
+        createdAt: "2024-05-29T10:41:30.630Z",
       },
     
       {
